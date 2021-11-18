fe3660c3d728f9c4675cfc81cfd139932a9700e8
wiktorsadowski <wiktor.sadowski@landingi.com>
2021-11-19 00:17:36 +0100

    Another quick fix ;)

--- src/User.php
+++ src/User.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+namespace Source;
+
 class User
 {
     private string $name;
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+use Source\User;
 
 class UserTest extends \PHPUnit\Framework\TestCase
 {
